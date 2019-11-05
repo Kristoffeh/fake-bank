1a0218c531627939103393ea88f062035e56671a
Kristoffer <kriss0706@hotmail.com>
2019-11-05 21:56:00 +0100

    Added core version
    Added static text to show core version

--- portal/home.php
+++ portal/home.php
@@ -73,6 +73,11 @@ if (!$r){
                         <button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button">
                             <i class="fas fa-bars"></i>
                         </button>
+                        <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                            <div class="input-group">
+                                <label style="font-size: 10px;">Core v1</label>
+                            </div>
+                        </form>
                         <ul class="nav navbar-nav flex-nowrap ml-auto">
                             <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><i class="fas fa-search"></i></a>
                                 <div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" role="menu" aria-labelledby="searchDropdown">
